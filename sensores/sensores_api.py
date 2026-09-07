a1efb93f952f9a6d84d80647e8af0a27a8dec698
jainerchocue <johanychocue@gmail.com>
2026-09-07 18:50:42 -0500

    feat(sensores): servicio base de sensores

--- sensores/sensores_api.py
+++ sensores/sensores_api.py
@@ -1,10 +1,8 @@
-from flask import Flask 
+from flask import Flask, request
 
 sensor = Flask(__name__)
 
-@sensor.route('/sensores', methods=["GET"])
-def datos_sensor():
-    datos = [
+datos = [
         {
             "id":1,
             "tipo":"Ph",
@@ -26,6 +24,45 @@ def datos_sensor():
             "estado":"Temperatura alta"
         }
     ]
-
+@sensor.route('/sensores', methods=["GET"])
+def datos_sensor():
+    if not datos: 
+        return {"mensaje":"No hay datos"}
     return datos
 
+
+@sensor.route("/sensores/<id>", methods=["GET"])
+def datos_sensor_id(id: int):
+
+    for i  in datos:
+        if i['id']== int(id):
+            return {
+                "id":i["id"],
+                "tipo": i["tipo"],
+                "ubicacion": i["ubicacion"],
+                "estado": i["estado"]
+            }, 200
+        
+    if not datos:
+        return {"mensaje":"No hay datos"}, 404
+    
+    
+    return {"mensaje":f"El usuario con el id {id}, no existe"}, 404
+
+
+
+@sensor.route("/sensores", methods=["POST"])
+def registrar_datos():
+    mayor = 0
+    valores = request.get_json()
+    for i in datos:
+        if i["id"] > mayor:
+            mayor = i["id"]
+    valores["id"] = mayor + 1
+    datos.append(valores)
+    return valores, 201
+
+
+
+if __name__ == "__main__":
+    sensor.run(host="0.0.0.0", port=5000, debug=True)
