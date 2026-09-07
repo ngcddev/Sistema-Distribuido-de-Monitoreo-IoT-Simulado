43d4cbda6dc41bf67e8a747399b7dc2a7ab8dfbe
ngcddev <ngaviriaceron@gmail.com>
2026-09-06 21:40:15 -0500

    docs: requirementes de servicio monitoreo

--- mediciones/app.py
+++ mediciones/app.py
@@ -37,4 +37,4 @@ def obtener_mediciones():
     return jsonify(resultado), 200
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5001) 
+    app.run(host="localhost", port=5001) # cambiar a host="0.0.0.0" para poder dockerizar
